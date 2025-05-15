df40e8831f6468149e92c38c05767c736ed658e1
muhammadNameer <nameerfarooquit@gmail.com>
2025-05-15 15:11:03 +0500

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -10,21 +10,21 @@ import {
 } from "@livekit/components-react";
 import { Room, Track } from "livekit-client";
 import "@livekit/components-styles";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { IoIosSend } from "react-icons/io";
 import { FaUserAlt } from "react-icons/fa";
+import { AiOutlineFullscreen, AiOutlineFullscreenExit } from "react-icons/ai";
 
 const serverUrl = "wss://solo-9dwvrt7c.livekit.cloud";
-// const token =
-//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDY5MTQwNzcsImlzcyI6IkFQSVhvdEhaNGkzZkNWOCIsIm5iZiI6MTc0NjkwNjg3Nywic3ViIjoicXVpY2tzdGFydCB1c2VyIDdnbnp1aCIsInZpZGVvIjp7ImNhblB1Ymxpc2giOnRydWUsImNhblB1Ymxpc2hEYXRhIjp0cnVlLCJjYW5TdWJzY3JpYmUiOnRydWUsInJvb20iOiJxdWlja3N0YXJ0IHJvb20iLCJyb29tSm9pbiI6dHJ1ZX19.2oACsUCjIcBN0gFOCaJtD088vv8YpN2BFoofzK8pRuA";
-// const token =
-//   "eyJhbGciOiJIUzI1NiJ9.eyJ2aWRlbyI6eyJyb29tSm9pbiI6dHJ1ZSwicm9vbSI6InF1aWNrc3RhcnQtcm9vbSJ9LCJpc3MiOiJBUElYb3RIWjRpM2ZDVjgiLCJleHAiOjE3NDcwNDIxMjYsIm5iZiI6MCwic3ViIjoicXVpY2tzdGFydC11c2VybmFtZSJ9._DRAWU5Cq7SNqSX738Mh3FGvXkQub3Y-kyFkRZeFyGg";
 
 export default function App() {
   const [token, settoken] = useState("");
-  const [participantName, setparticipantName] = useState("aaaa");
+  const [participantName, setparticipantName] = useState("nameer");
   const [showVideo, setshowVideo] = useState(false);
   const [isCreator, setisCreator] = useState(true);
+  const [isJoining, setisJoining] = useState(false);
+  const creator = "nameer";
+
   const [room] = useState(
     () =>
       new Room({
@@ -48,6 +48,7 @@ export default function App() {
   const getToken = async () => {
     if (participantName) {
       try {
+        setisJoining(true);
         const res = await fetch("http://localhost:3001/getToken", {
           method: "POST",
           headers: {
@@ -59,6 +60,7 @@ export default function App() {
         settoken(data.token);
       } catch (error) {
         console.error("Error fetching token:", error);
+        setisJoining(false);
       }
     }
   };
@@ -67,24 +69,44 @@ export default function App() {
   useEffect(() => {
     let mounted = true;
     const connect = async () => {
-      if (mounted && token) {
-        await room
-          .connect(serverUrl, token, {
-            audio: true,
-            video: true,
-          })
-          .then(() => {
-            setshowVideo(true);
-          })
-          .catch(() => {
-            setshowVideo(false);
-          });
+      try {
+        setisJoining(true);
+        if (mounted && token) {
+          await room
+            .connect(serverUrl, token, {
+              audio: true,
+              video: true,
+            })
+            .then(() => {
+              setshowVideo(true);
+            })
+            .catch(() => {
+              setshowVideo(false);
+            });
+        }
+        setisJoining(false);
+      } catch (error) {
+        console.log("Connect() error: ", error);
+        setisJoining(false);
       }
     };
 
     connect();
-
-    console.log("room events : ", room.eventNames);
+    room.on("disconnected", (reason) => {
+      console.log("disconnected event listener : ", reason);
+      room.disconnect();
+      settoken("");
+      setshowVideo(false);
+    });
+    room.on("participantDisconnected", (participant) => {
+      console.log("participant disconnected : ", participant);
+      if (participant.identity === creator) {
+        console.log("DESTROY");
+        room.disconnect();
+        settoken("");
+        setshowVideo(false);
+      }
+    });
 
     return () => {
       mounted = false;
@@ -97,18 +119,25 @@ export default function App() {
       {showVideo ? (
         <RoomContext.Provider value={room} connect={true}>
           <div data-lk-theme="default" className="w-full">
-            <MyVideoConference />
+            <MyVideoConference participantName={participantName} />
           </div>
         </RoomContext.Provider>
       ) : (
         <>
+          <div className="flex bg-baseColor rounded-md items-center justify-center w-full h-[70vh]">
+            <div
+              onClick={getToken}
+              className="py-[10px] px-[40px] cursor-pointer rounded-md bg-yellow2 text-black font-bold"
+            >
+              {isJoining ? "Joining..." : "Join livestreaming"}
+            </div>
+          </div>
           <input
             placeholder="Enter your name"
             type="text"
             onChange={(e) => setparticipantName(e.target.value)}
             value={participantName}
           />
-          <button onClick={getToken}>Connect</button>
           Creator :{" "}
           <input
             type="checkbox"
@@ -122,44 +151,7 @@ export default function App() {
   );
 }
 
-// function MyVideoConference() {
-//   // `useTracks` returns all camera and screen share tracks. If a user
-//   // joins without a published camera track, a placeholder track is returned.
-//   // const tracks = useTracks(
-//   //   [
-//   //     { source: Track.Source.Camera, withPlaceholder: true },
-//   //     { source: Track.Source.ScreenShare, withPlaceholder: false },
-//   //   ],
-//   //   { onlySubscribed: false }
-//   // );
-//   const participants = useParticipants();
-//   // const { chatMessages, send, isSending } = useChat();
-
-//   console.log("all participants ", participants);
-//   return (
-//     <>
-//       <div style={{ color: "black" }}>
-//         {`participants : ${participants.length}`}
-//         {participants.map((participant) => {
-//           console.log("participants ", participant);
-//           return <p>{participant.identity}</p>;
-//         })}
-//       </div>
-//       <div data-lk-theme="default" style={{ height: "100vh" }}>
-//         <VideoConference>
-//           {/* <Chat>
-//             {chatMessages.map((msg) => (
-//               <ChatEntry hideName={false} hideTimestamp={false} entry={msg} />
-//             ))}
-//           </Chat> */}
-//         </VideoConference>
-
-//         <ChatComponent />
-//       </div>
-//     </>
-//   );
-// }
-function MyVideoConference() {
+function MyVideoConference({ participantName }) {
   const creator = "nameer";
   const participants = useParticipants();
   const tracks = useTracks([
@@ -173,15 +165,15 @@ function MyVideoConference() {
   const screenShareTracks = tracks.filter(
     (t) => t.source === Track.Source.ScreenShare
   );
+
+  const [showCameraWhileScreenShare, setshowCameraWhileScreenShare] =
+    useState(true);
   return (
     <div
       data-lk-theme="default"
-      className="relative max-h-[70vh] flex flex-wrap lg:flex-nowrap  gap-2 w-screen "
+      className="relative max-h-[70vh] flex flex-wrap lg:flex-nowrap  gap-2 w-full "
     >
-      {/* <div style={{ color: "black" }}>
-        {("participants : ", participants.length)}
-      </div> */}
-      <div className="flex-[1] lg:flex-[3] flex flex-col relative min-h-[70vh]">
+      <div className="flex-[1] lg:flex-[3] flex flex-col relative min-h-[70vh] rounded-md">
         <div className="absolute top-[10px] left-[10px] z-[99] flex rounded-md bg-darkGray opacity-85 text-white py-[5px] px-[10px] gap-[5px] items-center">
           <FaUserAlt color="#eda803" />
           <p className="text-[14px] font-bold">{participants?.length}</p>
@@ -200,31 +192,44 @@ function MyVideoConference() {
               <ParticipantTile />
             </GridLayout>
 
+            {!showCameraWhileScreenShare ? (
+              <abbr title="Show camera view">
+                <div
+                  onClick={() => {
+                    setshowCameraWhileScreenShare(!showCameraWhileScreenShare);
+                  }}
+                  className="flex opacity-85 items-center gap-[3px] justify-center absolute top-[10px] right-[10px] cursor-pointer z-20 w-[70px] h-[30px] bg-lightGray text-white rounded-full"
+                >
+                  <FaUserAlt color="#fff" />
+                  <AiOutlineFullscreen color="#eda803" size={24} />
+                </div>
+              </abbr>
+            ) : (
+              <div className=" rounded-md border-2 border-[#eda803] z-10 hidden sm:block h-[50px] sm:h-[150px] w-[50px] sm:w-[200px] absolute right-[10px] top-[10px]">
+                <abbr title="Hide camera view">
+                  <div
+                    onClick={() => {
+                      setshowCameraWhileScreenShare(
+                        !showCameraWhileScreenShare
+                      );
+                    }}
+                    className="flex items-center justify-center absolute -top-[10px] -left-[10px] cursor-pointer z-20 w-[30px] h-[30px] bg-lightGray text-white rounded-full"
+                  >
+                    <AiOutlineFullscreenExit color="#eda803" size={24} />
+                  </div>
+                </abbr>
+
+                <GridLayout
+                  style={{
+                    backgroundColor: "#142028",
+                  }}
+                  tracks={cameraTracks}
+                >
+                  <ParticipantTile />
+                </GridLayout>
+              </div>
+            )}
             {/* Small camera overlay */}
-            <div
-              // style={{
-              //   position: "absolute",
-              //   top: 10,
-              //   right: 10,
-              //   width: "200px",
-              //   height: "150px",
-              //   zIndex: 10,
-              //   border: "2px solid #eda803",
-              //   backgroundColor: "#142028",
-              //   borderRadius: "8px",
-              //   overflow: "hidden",
-              // }}
-              className="overflow-hidden rounded-md border-2 border-[#eda803] z-10 hidden sm:block h-[50px] sm:h-[150px] w-[50px] sm:w-[200px] absolute right-[10px] top-[10px]"
-            >
-              <GridLayout
-                style={{
-                  backgroundColor: "#142028",
-                }}
-                tracks={cameraTracks}
-              >
-                <ParticipantTile />
-              </GridLayout>
-            </div>
           </>
         ) : (
           // No screen share, show cameras in full layout
@@ -241,41 +246,58 @@ function MyVideoConference() {
         )}
 
         {/* Controls */}
-        <div className="bg-baseColor">
+        <div className="bg-baseColor rounded-b-md">
           <ControlBar />
         </div>
       </div>
 
       {/* Chat */}
       <div className="md:flex-[1] w-full  md:min-w-[300px]">
-        <ChatComponent />
+        <ChatComponent participantName={participantName} />
       </div>
     </div>
   );
 }
 
-function ChatComponent() {
+function ChatComponent({ participantName }) {
   const creator = "nameer";
 
   const { chatMessages, send, isSending } = useChat();
   const [msg, setmsg] = useState("");
-
+  const bottomRef = useRef(null);
+  useEffect(() => {
+    if (bottomRef.current) {
+      bottomRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [chatMessages]);
   return (
     <div className="rounded-md text-white bg-baseColor bg-opacity-75 backdrop-blur-3xl flex flex-col gap-2">
       <div className="flex font-bold text-[20px]  p-[10px] bg-darkGray">
         Live Chat
       </div>
-      <div className="flex flex-col gap-2 p-[15px] h-[53vh] overflow-auto">
+      <div className="flex flex-col gap-2 p-[15px] h-[53vh] overflow-auto custom-scrollbar">
         {chatMessages?.map((entry, index) => (
-          <div key={index} className="flex w-full">
-            <div className="flex flex-col bg-lightGray min-w-[280px] max-w-[400px] rounded-md px-[15px] pt-[10px] pb-[25px] relative">
-              <p className="font-bold text-[18px] text-yellow2">
+          <div
+            key={index}
+            className={`flex w-full ${
+              participantName === (entry.from.name || entry.from?.identity) &&
+              "justify-end"
+            }`}
+          >
+            <div
+              className={`flex flex-col ${
+                participantName === (entry.from.name || entry.from?.identity)
+                  ? "bg-lightGray"
+                  : "bg-lightGray"
+              }  min-w-[200px] max-w-[400px] rounded-md px-[10px] pt-[5px] pb-[25px] relative`}
+            >
+              <p className="font-medium text-[16px] text-yellow2">
                 {entry.from?.name || entry.from?.identity || "Unknown"}{" "}
                 {(entry.from?.name || entry.from?.identity) === creator && (
-                  <span className="text-[14px] text-white">(Creator)</span>
+                  <span className="text-[12px] text-white">(Creator)</span>
                 )}
               </p>
-              <p className="text-[16px] break-words overflow-wrap break-word whitespace-pre-wrap">
+              <p className="text-[14px] break-words overflow-wrap break-word whitespace-pre-wrap">
                 {entry.message}
               </p>
               <p className="text-[12px] absolute bottom-[2px] right-[5px] text-disableTextColor">
@@ -284,6 +306,7 @@ function ChatComponent() {
             </div>
           </div>
         ))}
+        <div ref={bottomRef} />
       </div>
       <div className="flex gap-2 px-[15px] py-[15px]">
         <input
